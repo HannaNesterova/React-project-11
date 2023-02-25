00ab61a96070b6341bb41a8e9a0a351f59b37ff6
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-02-25 21:24:38 +0100

    Add the first text to an App.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+
+// ReactDOM.render(
+//   <App />,
+//   document.querySelector('#root')
+// );
+
+
+// import React from 'react';
+// import { createRoot } from 'react-dom';
+
+// createRoot(document.getElementById('root')).render(<App />);
+
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+createRoot(document.getElementById('root')).render(<App />);
