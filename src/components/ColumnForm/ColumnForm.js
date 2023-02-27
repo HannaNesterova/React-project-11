7eda28e10f2ea048577973f351b31d90de861a23
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-02-27 22:54:08 +0100

    Add temples to (e)

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,22 +1,32 @@
+import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
 
-
-
 const ColumnForm = props => {
-    const [value, setValue] = useState('');
-    const addColumn = e => {
+    //const [value, setValue] = useState('');
+    const [title, setTitle] = useState('');
+    const [icon, setIcon] = useState('');
+
+    /*const addColumn = e => {
         e.preventDefault();
         props.action({ title: value });
         setValue('');
-    }
+    };*/
+  
+
+    const addColumn = (e) => {
+        e.preventDefault();
+        props.action({ title: title, icon: icon });
+        setTitle('');
+        setIcon('');
+};
 
 	return (
         <form onSubmit={addColumn}>
-            <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+           <span> Title: </span> <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+           <span>Icon: </span><input type="text" value={icon} onChange={(e) => setIcon(e.target.value)} />
             <button>Add column</button>
         </form>
 	);
 };
-
 
 export default ColumnForm;
