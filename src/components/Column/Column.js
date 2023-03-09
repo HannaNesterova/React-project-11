917c060f4f922fca3aaf7173fcc5f43914c02d11
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-03-09 12:55:36 +0100

    ensure that props.cards is defined and contains an array of objects before trying to map over it

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -7,7 +7,8 @@ const Column = props => {
     <article className={styles.column}>
         <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon} />{props.title}</h2>
         <ul className={styles.cards}>
-            {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+            {props.cards && props.cards.map(card => <Card key={card.id} title={card.title} />)}
+            {/* {props.cards.map(card => <Card key={card.id} title={card.title} />)} */}
             <CardForm columnId={props.id} action={props.action} />
         </ul>
     </article>);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,10 @@
 import { createStore } from 'redux';
+import initialState from './initialState';
+
 const reducer = (state, action) => {
   return state;
 };
 
-const initialState = {
-  columns: []
-};
 
 const store = createStore(
   reducer,
